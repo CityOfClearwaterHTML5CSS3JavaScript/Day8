74599fc2f14ef85ec54a48b9ec46e6899d7e1e51
Jeff McBride <jeff@geeklearn.com>
2014-09-10 07:41:40 -0400

    Added Files for Day 8

---
+++ AttachDB.sql
@@ -0,0 +1,7 @@
+﻿USE [master]
+GO
+CREATE DATABASE [MyClearwater.com] ON 
+( FILENAME = N'C:\Users\Geek\Documents\City of Clearwater\Projects\myclearwater.com\MyClearwater.com.mdf' ),
+( FILENAME = N'C:\Users\Geek\Documents\City of Clearwater\Projects\myclearwater.com\MyClearwater.com.ldf' )
+ FOR ATTACH ;
+GO
